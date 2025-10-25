41b3e84484da54bfa7d1aa301e29df9636cd82ce
Erick Gabriel Peroza <perozaerick@gmail.com>
2025-10-25 15:59:42 -0300

    feat: implementado uma nova validação da existência do arquivo challenge.xlsx, caso exista ele pula a etapa de baixar novamente

--- bot-desafio-tecnico-havan/bot.py
+++ bot-desafio-tecnico-havan/bot.py
@@ -5,10 +5,14 @@ from datetime import datetime
 
 import pandas as pd
 
+from webdriver_manager.firefox import GeckoDriverManager
+
 from botcity.maestro import BotMaestroSDK
 from botcity.web import WebBot, Browser, By
 
+
 DIRETORIO_RESOURCES = str(Path(__file__).resolve().parent / "resources")
+
 
 class DownloadExcel:
     def __init__(self):
@@ -18,25 +22,25 @@ class DownloadExcel:
 
         self.df = pd.DataFrame()
 
-
     def main(self):
         self.bot.headless = False
 
         self.bot.browser = Browser.FIREFOX
 
-        # TO DO
-        self.bot.driver_path = self.bot.get_resource_abspath("geckodriver.exe")
+        self.bot.driver_path = rf"{DIRETORIO_RESOURCES}/challenge.xlsx"
 
         self.bot.download_folder_path = DIRETORIO_RESOURCES
 
         self.bot.browse("https://www.rpachallenge.com/")
 
-        botao_download_excel = self.bot.find_element(
-            "//a[contains(@href, 'challenge.xlsx') and contains(@class, 'uiColorPrimary') and contains(normalize-space(text()), 'Download Excel')]",
-            By.XPATH,
-        )
-        botao_download_excel.click()
-        self.bot.wait(3000)
+        if not os.path.exists(rf"{DIRETORIO_RESOURCES}/challenge.xlsx"):
+            botao_download_excel = self.bot.find_element(
+                "//a[contains(@href, 'challenge.xlsx') and contains(@class, 'uiColorPrimary') and contains(normalize-space(text()), 'Download Excel')]",
+                By.XPATH,
+            )
+
+            botao_download_excel.click()
+            self.bot.wait(3000)
 
         botao_start = self.bot.find_element(
             "//button[contains(@class, 'uiColorButton') and normalize-space(text())='Start']",
@@ -44,12 +48,10 @@ class DownloadExcel:
         )
         botao_start.click()
 
-        self.df = pd.read_excel(fr"{DIRETORIO_RESOURCES}/challenge.xlsx")
+        self.df = pd.read_excel(rf"{DIRETORIO_RESOURCES}/challenge.xlsx")
 
-        self.df.columns = (
-            self.df.columns
-            .str.strip()
-            .str.replace(r"\s+", " ", regex=True)
+        self.df.columns = self.df.columns.str.strip().str.replace(
+            r"\s+", " ", regex=True
         )
 
         for _, row in self.df.iterrows():
@@ -68,7 +70,9 @@ class DownloadExcel:
             )
             nome_empresa.send_keys(row["Company Name"])
 
-            cargo = self.bot.find_element("//input[@ng-reflect-name='labelRole']", By.XPATH)
+            cargo = self.bot.find_element(
+                "//input[@ng-reflect-name='labelRole']", By.XPATH
+            )
             cargo.send_keys(row["Role in Company"])
 
             endereco = self.bot.find_element(
@@ -76,7 +80,9 @@ class DownloadExcel:
             )
             endereco.send_keys(row["Address"])
 
-            email = self.bot.find_element("//input[@ng-reflect-name='labelEmail']", By.XPATH)
+            email = self.bot.find_element(
+                "//input[@ng-reflect-name='labelEmail']", By.XPATH
+            )
             email.send_keys(row["Email"])
 
             numero_telefone = self.bot.find_element(
@@ -93,12 +99,15 @@ class DownloadExcel:
         self.bot.wait(3000)
         self.bot.stop_browser()
 
-
     def _criar_log(self):
         os.makedirs("logs", exist_ok=True)
         nome_arquivo_log = datetime.now().strftime("logs/execucao_%d-%m-%Y.log")
 
-        logging.basicConfig(filename=nome_arquivo_log, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+        logging.basicConfig(
+            filename=nome_arquivo_log,
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+        )
 
 
 if __name__ == "__main__":
